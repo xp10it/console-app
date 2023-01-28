f8e4508a85fb17f26e8865363b0369846bda4ea5
Artem Timokhin <960fischer@gmail.com>
2023-01-28 22:08:18 +0600

    Implement user input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isRunning = true;
+
+        while(isRunning) {
+            String input;
+            byte[] buffer = new byte[64];
+
+            try {
+                int read = System.in.read(buffer, 0, 64);
+                input = new String(buffer, 0, read).strip();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+
+                isRunning = false;
+
+                continue;
+            }
+
+            System.out.println("User input " +  input);
+
+            if (input.equals("-q")) {
+                isRunning = false;
+            }
+        }
     }
 }
